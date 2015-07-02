74af01870ac29795203da5aa35ea802b3f275564
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-07-02 13:09:12 -0700

    Fix

--- Makestagram/Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/Makestagram/ViewControllers/TimelineViewController.swift
@@ -2,11 +2,12 @@
 //  TimelineViewController.swift
 //  Makestagram
 //
-//  Created by Praynaa Rawlani on 6/30/15.
+//  Created by Arun Rawlani on 6/30/15.
 //  Copyright (c) 2015 Make School. All rights reserved.
 //
 
 import UIKit
+import Parse
 
 class TimelineViewController: UIViewController {
     
@@ -21,7 +22,15 @@ class TimelineViewController: UIViewController {
     func takePhoto(){
         // instantiate photo taking class, provide callback for when photo is selected
         photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!){ (image : UIImage?) in
-            println("received an image. Legen-wait-for-it-dary!")
+            let imageData = UIImageJPEGRepresentation(image, 0.8)
+            let imageFile = PFFile(data: imageData)
+            imageFile.save()
+            
+            let post = PFObject(className: "Post")
+            post["imageFile"] = imageFile
+            post.save()
+            
+            println("Image uploaded. Hips-da-boss!")
         }
     }
     }
