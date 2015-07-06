2c75ef2fb6c61fa0ef43dd50e572b87731539d93
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-07-05 19:08:14 -0700

    Added PostTableView Class and added Image View in mainstoryboard

---
+++ Makestagram/Makestagram/Views/PostTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Praynaa Rawlani on 7/5/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+
+
