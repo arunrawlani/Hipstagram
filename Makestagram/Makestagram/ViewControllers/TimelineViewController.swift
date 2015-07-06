66b92e2afe6eb155657b1436f42f39440be46300
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-07-05 18:36:04 -0700

    Building the TimeLine Query including queries for returning followers and their posts and then displaying them in ascending order by time. Also adding code for reloading timeline over time new posts are added

--- Makestagram/Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/Makestagram/ViewControllers/TimelineViewController.swift
@@ -14,6 +14,7 @@ class TimelineViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,36 @@ class TimelineViewController: UIViewController {
         self.tabBarController?.delegate = self
         }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //Fetches follow relationships for current users
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo: PFUser.currentUser()!)
+        
+        //Using previous query to fetch any posts made by followers of currentUser
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        //Another query to retunr all posts made by the current user
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        //creates a query that returns all posts from the previous 2 queries
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        //extracting information to display with the post
+        query.includeKey("user")
+        query.includeKey("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock {(result: [AnyObject]?, error: NSError?) -> Void in
+            
+            //casts to type Post array or just returns an empty array
+            self.posts = result as? [Post] ?? []
+            //refresh tableView
+            self.tableView.reloadData()
+    }
+    }
+
     func takePhoto(){
         // instantiate photo taking class, provide callback for when photo is selected
         photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!){ (image : UIImage?) in
@@ -49,4 +80,21 @@ extension TimelineViewController: UITabBarControllerDelegate {
         }
     }
    
+}
+
+extension TimelineViewController: UITableViewDataSource{
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //Table View should have the same amount of rows as the number of posts stored in the array
+    return posts.count
+}
+
+func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! UITableViewCell
+    //return a simple placeholder cell with title Post
+    cell.textLabel!.text = "Post"
+    
+    return cell
+    }
 }
